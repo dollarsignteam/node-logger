f7f1dc2e38caa29b8c8c93d641a4eafb3cb5b776
chagridsada <chagridsada@dollarsign.co>
2021-06-21 14:20:24 +0700

    test(utils): add winston spec

--- src/utils/spec/winston.spec.ts
+++ src/utils/spec/winston.spec.ts
@@ -1,5 +1,46 @@
-describe('logger', () => {
-  it('should to be defined', () => {
-    expect(true).toBeDefined();
+import { TransformableInfo } from 'logform';
+
+import { LoggerOptions, LogLevels } from '../../constants';
+import { createWinstonLogger, templateFactory } from '../winston';
+
+describe('winston', () => {
+  describe('createWinstonLogger', () => {
+    it('should return default logger', () => {
+      const logger = createWinstonLogger();
+      expect(logger.level).toBe(LogLevels.silly);
+    });
+
+    it('should return custom logger', () => {
+      const options: LoggerOptions = {
+        level: 'warn',
+        name: 'TEST',
+        platform: 'node',
+      };
+      const logger = createWinstonLogger(options);
+      expect(logger.level).toBe(LogLevels.warn);
+    });
+  });
+
+  describe('templateFactory', () => {
+    it('should return string with `data`', () => {
+      const info: TransformableInfo = {
+        level: LogLevels.debug,
+        label: '[TEST]',
+        message: 'foo',
+        ...{ foo: 'bar' },
+      };
+      const template = templateFactory(info);
+      expect(template).toBe('[TEST] debug: foo - `{"foo":"bar"}`');
+    });
+
+    it('should return string with out `data`', () => {
+      const info: TransformableInfo = {
+        level: LogLevels.debug,
+        label: '[TEST]',
+        message: 'foo',
+      };
+      const template = templateFactory(info);
+      expect(template).toBe('[TEST] debug: foo');
+    });
   });
 });
