374097a01a195070265ac2495b3f2b3c459390f9
chagridsada <chagridsada@dollarsign.co>
2021-06-24 13:38:47 +0700

    fix(utils): change default timestamp format

--- src/utils/get-timestamp.ts
+++ src/utils/get-timestamp.ts
@@ -1,10 +1,9 @@
-import { isEmpty } from '@dollarsign/utils';
 import { format } from 'fecha';
 
 /**
  * @param {string} mask string `fecha` date formatting
- * @returns {string} timestamp default is `toISOString` format
+ * @returns {string} timestamp default is `YYYY-MM-DD HH:mm:ss.SSS Z` format
  */
 export function getTimestamp(mask?: string): string {
-  return isEmpty(mask) ? new Date().toISOString() : format(new Date(), mask);
+  return format(new Date(), mask || 'YYYY-MM-DD HH:mm:ss.SSS Z');
 }
--- src/utils/spec/get-timestamp.spec.ts
+++ src/utils/spec/get-timestamp.spec.ts
@@ -1,11 +1,9 @@
 import { getTimestamp } from '../get-timestamp';
 
 describe('getTimestamp', () => {
-  it('should return ISO string format', () => {
-    const expected = '2021-06-22T12:15:08.973Z';
-    jest.spyOn(Date.prototype, 'toISOString').mockReturnValueOnce(expected);
+  it('should return `YYYY-MM-DD HH:mm:ss.SSS Z` string format', () => {
     const result = getTimestamp();
-    expect(result).toBe(expected);
+    expect(result).toMatch(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3} \+\d{2}:\d{2}/);
   });
 
   it('should return custom format', () => {
